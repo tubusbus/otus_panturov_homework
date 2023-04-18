b90311268e16bfc471a279bb8dc343d0c1b6c318
tubusbus <panturikow@gmail.com>
2023-04-19 00:27:21 +0300

    .

--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.conditions.webdriver.Url;
 import org.example.driver.DriverFactory;
 import org.example.pages.MainPage;
 import org.junit.jupiter.api.*;
@@ -6,6 +7,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.example.enums.PagesEnum.LESSONS_PAGE;
 import static org.example.enums.PagesEnum.MAIN_PAGE;
@@ -18,12 +27,31 @@ public class MainPageTest {
   private WebDriver driver;
 
   public MainPageTest() {
-    driver = new DriverFactory().getDriver();
-    this.page = new MainPage(driver);
+//    driver = new DriverFactory().getDriver();
+//    this.page = new MainPage(driver);
   }
 
   @BeforeEach
   public void preface() {
+//     @BeforeEachvoid setupTest() throws MalformedURLException {
+    Map<String, Object> selenoidOptions = new HashMap<>();
+    selenoidOptions.put("enableVNC", true);
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("browserName", "chrome");
+    capabilities.setCapability("browserVersion", "109.0");
+    capabilities.setCapability("enableVideo", false);
+    capabilities.setCapability("selenoid:options", selenoidOptions);
+    driver = null;
+    try {
+      driver = new RemoteWebDriver(
+              URI.create("http://127.0.0.1:4444/wd/hub").toURL(), capabilities
+      );
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+//  }
+    this.page = new MainPage(driver);
+
     page.openPage(MAIN_PAGE);
     page.checkLoadPage();
   }
@@ -35,7 +63,7 @@ public class MainPageTest {
 
   @ParameterizedTest(name = "{index} - Выбор курса названием {0}")
   @CsvSource({
-          "BI-аналитика,/bi-analytics-course/",
+//          "BI-аналитика,/bi-analytics-course/",
           "Специализация iOS Developer,/ios-specialization/"
   })
   public void clickCourseByName(String courseName, String expectedUrl) {
